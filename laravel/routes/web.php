1eeb63541d932d2bfb83411f9add635930d04006
2joaopedro <joao.pedro.franchini.4@gmail.com>
2022-07-12 19:44:58 -0300

    fet: criando rota com parametro

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -2,17 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/',[\App\Http\Controllers\Principal_Controller::class,'principal']);
 Route::get('/sobre-nos',[\App\Http\Controllers\Sobre_Nos_Controller::class,'sobrenos']);
 Route::get('/contato',[App\Http\Controllers\Contato_Controller::class, 'contato']);
+
+route::get('/contato/{nome}/{categoria}/{assunto}/{mensagem}', function(string $nome, string $categoria, string $assunto, string
+$mensagem){
+  echo "Estamos aqui: $nome - $categoria - $assunto - $mensagem";
+
+});
