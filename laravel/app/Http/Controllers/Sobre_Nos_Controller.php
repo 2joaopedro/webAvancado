1827fb2b12599715ba759e86cf63aba01f89bd93
2joaopedro <joao.pedro.franchini.4@gmail.com>
2022-07-11 22:48:40 -0300

    fet: criei o crontroller do (Contao, Principal e Sobrenos)

---
+++ laravel/app/Http/Controllers/Sobre_Nos_Controller.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Sobre_Nos_Controller extends Controller
+{
+    public function sobrenos(){
+        echo 'Bem vindo no sobre nos!';
+    }
+}
