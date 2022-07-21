25e50309af93158760faa98bcebaa09a19b5be83
João <joao.pedro.franchini.4@gmail.com>
2022-07-21 16:54:42 -0300

    Fet: controller,views e rotas

--- laravel/resources/views/sobrenos.blade.php
+++ laravel/resources/views/sobrenos.blade.php
@@ -1,1 +1,7 @@
-<h1>oi</h1>
+<h1>Sobre Nós (view)</h1>
+
+<ul>
+  <li><a href="{{route('site.index')}}">Principal</a></li>
+  <li><a href="{{route('site.sobrenos')}}">Sobre Nós</a></li>
+  <li><a href="{{route('site.contato')}}">Contato</a></li>
+</ul>
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -2,11 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/',[\App\Http\Controllers\Principal_Controller::class,'principal']);
-Route::get('/sobre-nos',[\App\Http\Controllers\Sobre_Nos_Controller::class,'sobrenos']);
-Route::get('/contato',[App\Http\Controllers\Contato_Controller::class, 'contato']);
+Route::get('/',[\App\Http\Controllers\Principal_Controller::class,'principal'])->name('site.index');
+Route::get('/sobre-nos',[\App\Http\Controllers\Sobre_Nos_Controller::class,'sobrenos'])->name('site.sobrenos');
+Route::get('/contato',[App\Http\Controllers\Contato_Controller::class, 'contato'])->name('site.contato');
+Route::get('/login', function(){return 'Login';})->name('site.login');
 
-route::get('/contato/{nome}/{categoria}/{assunto?}', function(string $nome, string $categoria, string $assunto = 'assunto não informada'){
-  echo "Estamos aqui: $nome - $categoria - $assunto";
+Route::prefix('/app')->group(function(){
+Route::get('/clientes', function(){return 'Clientes';})->name('app.clientes');
+Route::get('/fornecedores', function(){return 'Fornecedores';})->name('app.fornecedores');
+Route::get('/produtos', function(){return 'Produtos';})->name('app.produtos');
+});
 
+Route::get('/teste/{p1}/{p2}',[\App\Http\Controllers\Teste_Controller::class,'teste'])->name('teste');
+/*
+Route::get('/rota2', function(){
+  return redirect()->route('site.rota1');
+})->name('site.rota2');
+*/
+
+//Route::redirect('rota2', '/rota1');
+
+Route::fallback(function(){
+  echo 'A rota acessada não existe. <a href="'.route('site.index').'">clique aqui</a> para ir para página inicial';
 });
